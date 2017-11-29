bc9e3eca0b364b400c9c1b18740af5ecb1da34ee
ihsavru <vurvashi11@gmail.com>
2017-11-29 13:52:54 +0530

    added quick replies and built-in nlp

--- echoBot/messengerEchoBot/views.py
+++ echoBot/messengerEchoBot/views.py
@@ -30,3 +30,4 @@ def post_facebook_message(fbid, recevied_message):
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
+
--- languageBot/messengerBot/views.py
+++ languageBot/messengerBot/views.py
@@ -4,7 +4,9 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json, requests
 from pprint import pprint
+import random
 
+page_access_token = 'EAAUDpluM64kBAHUIpJLUuZAQkLByg95Om3aR0QZADw9FBNkSUqCpN6RV56r6BLBHVMbzP99JQiRIaXDmVsjjhVYSt3HQMwZBqVyROE8ZAVTUdgWnxu7pm2Cxcp7JWecqxNLklxJTwwq0amnqmvCZBsIZABOos6XKdHkM3UlwCAndYZBoLHGWr9D'
 
 class messengerBotView(generic.View):
     @method_decorator(csrf_exempt)
@@ -19,22 +21,143 @@ class messengerBotView(generic.View):
 
     def post(self, request, *args, **kwargs):
         incoming_message = json.loads(self.request.body.decode('utf-8'))
+        get_started()
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 if 'message' in message:
                     pprint(message)
-                    post_facebook_message(message['sender']['id'], message['message']['text'])
+                    if message['sender']['id'] != '351106498695933':
+                        seen_message(message['sender']['id'])
+                        typing_message(message['sender']['id'])
+                        post_facebook_message(message['sender']['id'], message['message']['text'], message['message'])
         return HttpResponse()
 
-def post_facebook_message(fbid, received_message):
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAUDpluM64kBAHUIpJLUuZAQkLByg95Om3aR0QZADw9FBNkSUqCpN6RV56r6BLBHVMbzP99JQiRIaXDmVsjjhVYSt3HQMwZBqVyROE8ZAVTUdgWnxu7pm2Cxcp7JWecqxNLklxJTwwq0amnqmvCZBsIZABOos6XKdHkM3UlwCAndYZBoLHGWr9D'
-    response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": "Hello! Welcome to Language Learning Bot. This is a demo :) To begin learning choose from the following languages: German. Type 'German'."}})
+def seen_message(fbid):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + page_access_token
+    response_msg = json.dumps(
+        {
+            "recipient": {"id": fbid},
+            "sender_action": "mark_seen"
+                })
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
 
-    if received_message == "hi":
-        response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": "Hello! Hola! Bonjour! Namaste!"}})
+def typing_message(fbid):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + page_access_token
+    response_msg = json.dumps(
+        {
+            "recipient": {"id": fbid},
+            "sender_action": "typing_on"
+                })
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
+
+def firstEntity(nlp, name):
+    try:
+        return nlp and nlp['entities'] and nlp['entities'] and nlp['entities'][name] and nlp['entities'][name][0]
+    except:
+        return 0
+
+def post_facebook_message(fbid, received_message, message):
+    user_details_url = "https://graph.facebook.com/v2.6/%s" % fbid
+    user_details_params = {'fields': 'first_name,last_name', 'access_token': page_access_token}
+    user_details = requests.get(user_details_url, user_details_params).json()
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + page_access_token
+    response_msg = json.dumps(
+        {
+         "recipient": {"id": fbid},
+         "message": {"text": "Sorry, we didn't quite understand. Type 'Help' to see the available commands :)",
+         "quick_replies": [
+             {
+                 "content_type": "text",
+                 "title": "Help",
+                 "payload": "<STRING_SENT_TO_WEBHOOK>"
+             }]
+         }})
+
+    greeting = firstEntity(message['nlp'], 'greetings')
+    if greeting and greeting['confidence'] > 0.8:
+        greetings = [
+            "Hello! Hola! Bonjour! Namaste! To begin learning choose from the following languages: German. Type 'German'.",
+            "Hey "+ user_details['first_name']+ "! Type the name of the language to start learning",
+            'Welcome ' + user_details['first_name'] + ', to language learning bot! Click on one of the quick replies to begin learning.'
+        ]
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": random.choice(greetings),
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "German",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    }]
+            }})
+
+    thank = firstEntity(message['nlp'], 'thanks')
+    if thank and thank['confidence'] > 0.8:
+        thanks = [
+            'I hope you are enjoying the experience so far :)',
+            'No problem. It is my duty.',
+            'Hope you are enjoying learning.',
+            'You are welcome!'
+        ]
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": random.choice(thanks)
+            }})
+
+    bye = firstEntity(message['nlp'], 'bye')
+    if bye and bye['confidence'] > 0.8:
+        bye_message = [
+            'See you again ' + user_details['first_name'] + '. Have a great day!',
+            'Bye '+ user_details['first_name'] + '.',
+            'Hope you had a great time learning.'
+        ]
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": random.choice(bye_message)
+            }})
+
+    if received_message == "About":
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": "Hi! I am a demo bot written in Python (Django). I help you to learn languages. Currently I only know German.",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Help",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    }
+                ]
+            }})
 
     if received_message == "German":
-        response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": "Get ready to learn German! Type: Months, Weekdays, Numbers."}})
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": "Get ready to learn German! Currently available topics are: Months, Weekdays, Numbers.",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Months",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Weekdays",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Numbers",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    }
+                ]
+            }})
 
     if received_message == "Months":
         months = "The months are:\n Januar, Februar, Marz, April, Mai, Juni, Juli, August, September, Oktober, November, Dezember"
@@ -48,8 +171,47 @@ def post_facebook_message(fbid, received_message):
         numbers =" \n 1	eins \n 2	zwei \n 3	drei \n 4	vier \n 5	funf \n 6	sechs\n 7	sieben \n 8	acht \n 9	neun \n 10	zehn"
         response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": numbers}})
 
-    if received_message == "bye" or received_message == "Bye":
-        response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": "Bye! See you again :)"}})
+    if received_message == "Help":
+        response_msg = json.dumps({
+            "recipient": {"id": fbid},
+            "message": {
+                "text": "Try any of the following: ",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "About",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "German",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Months",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Weekdays",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Numbers",
+                        "payload": "<STRING_SENT_TO_WEBHOOK>"
+                    }
+                ]
+            }})
 
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
+
+def get_started():
+    post_message_url = 'https://graph.facebook.com/v2.6/me/threadsettings?accesstoken=' + page_access_token
+    response_msg = json.dumps({"get_started":{
+        "payload":"<GET_STARTED_PAYLOAD>"
+      }})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
